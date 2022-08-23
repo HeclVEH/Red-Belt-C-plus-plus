725bca1bd84886369e1f4742268ea5cc85358fc5
HeclVeh <eglet9999@gmail.com>
2022-08-23 21:30:03 +0700

    simple_vector

--- 3 week/simple_vector.h
+++ 3 week/simple_vector.h
@@ -6,19 +6,49 @@
 template <typename T>
 class SimpleVector {
 public:
-  SimpleVector();
-  explicit SimpleVector(size_t size);
-  ~SimpleVector();
+	SimpleVector() {
+		data = nullptr;
+		size_ = 0;
+		capacity_ = 0;
+	}
 
-  T& operator[](size_t index);
+	explicit SimpleVector(size_t size) : size_(size), capacity_(size) {
+		data = new T[size];			
+	}
 
-  T* begin();
-  T* end();
+	~SimpleVector() {
+		delete[] data;
+	}
 
-  size_t Size() const;
-  size_t Capacity() const;
-  void PushBack(const T& value);
+	T& operator[](size_t index) {
+		return data[index];
+	}
+
+	T* begin() { return data; }
+	T* end() { return data + size_; }
+
+	size_t Size() const { return size_; }
+	size_t Capacity() const { return capacity_; }
+
+	void PushBack(const T& value) {		
+		if (size_ == capacity_) {			
+			capacity_ *= 2;
+			if (size_ == 0) {
+				++capacity_;
+			}
+			T* result = new T[capacity_];
+			for (decltype(size_) i = 0; i < size_; ++i) {
+				result[i] = data[i];
+			}
+			delete[] data;
+			data = result;
+		}
+		data[size_++] = value;
+	}
 
 private:
-  // Добавьте поля для хранения данных вектора
+	T* data;	
+	size_t size_;
+	size_t capacity_;	
+	// Добавьте поля для хранения данных вектора
 };
